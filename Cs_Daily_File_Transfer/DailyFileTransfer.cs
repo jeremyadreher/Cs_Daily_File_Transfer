79687820b08d677372bdd89ce392eb0379a258ee
JeremyADreher <jeremyadreher@gmail.com>
2016-12-20 14:16:38 -0800

    Initial Commit: Program successfully moved files.

---
+++ Cs_Daily_File_Transfer/DailyFileTransfer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Cs_Daily_File_Transfer
+{
+    
+    class ModTimeCheck
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = "C:\\LocalSourceFiles\\Cs_Daily_File_Transfer\\Daily";
+            string destPath = "C:\\LocalSourceFiles\\Cs_Daily_File_Transfer\\Home_Office";
+            string fileExt = "*.txt";
+            string[] txtFiles = Directory.GetFiles(sourcePath, fileExt);
+
+            foreach (var file in txtFiles)
+            {
+                if (modTimeCheck(file))
+                {
+                    string fileName = Path.GetFileName(file);
+                    File.Copy(file, Path.Combine(destPath, fileName));
+
+                    Console.WriteLine("Copying ... {0}", fileName);
+                }
+            }
+
+            Console.WriteLine("\nFile Transfer Complete ... Press ENTER");
+            Console.ReadLine();
+        }
+
+        public static bool modTimeCheck(string fileName)
+        {
+            DateTime currentTime = DateTime.Now;
+
+            FileInfo fileNameInfo = new FileInfo(fileName);
+
+            if (fileNameInfo.LastWriteTime.AddDays(1) >= DateTime.Now)
+                return true;
+            else
+                return false;
+        }
+
+    }
+}
--- Cs_Daily_File_Transfer/Program.cs
+++ Cs_Daily_File_Transfer/Program.cs
@@ -3,13 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Cs_Daily_File_Transfer
 {
+    public class ModTimeCheck
+    {
+        public static bool modTimeCheck()
+        {
+            DateTime mT = File.GetLastWriteTime(sourcePath);
+            DateTime currentTime = DateTime.Now;
+
+            string[] txtFiles = Directory.GetFiles(sourcePath, fileExt);
+
+            if (mT.AddDays(1) >= currentTime)
+                return true;
+            else
+                return false;
+        }
+    }
+
+    public class MoveFiles
+    {
+        public string move_files()
+        {
+            string sourcePath = "C:\\LocalSourceFiles\\Cs_Daily_File_Transfer\\Daily";
+            string destPath = "C:\\LocalSourceFiles\\Cs_Daily_File_Transfer\\Home_Office";
+            string fileExt = "*.txt";
+
+            ModTimeCheck mTC = new ModTimeCheck();
+
+            if (mTC.modTimeCheck(true))
+            {
+                foreach (var item in txtFiles)
+                {
+                    File.Copy(item, Path.Combine(destPath, Path.GetFileName(item)));
+                }
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            MoveFiles mF = new MoveFiles();
+            mF.move_files();
+
+
+
+            Console.ReadKey();
         }
     }
 }
